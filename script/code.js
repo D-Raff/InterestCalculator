9521a0d95eeb2660278fcfc9f12f09dbe30d0025
D-Raff <damonraffels@gmail.com>
2023-11-24 15:04:11 +0200

    did functionality

--- script/code.js
+++ script/code.js
@@ -1,1 +1,19 @@
 // function
+
+// variable for the button
+let btnCalc = document.querySelector('[data-calc]')
+
+function Calculate() {
+    // input variable
+    let input = +document.querySelector('[data-amount]').value
+    // interest variable
+    let interest = +document.querySelector('[data-int]').value
+    // output variable
+    let output = document.querySelector('[data-res]')
+
+    let result = input * (interest / 100)
+
+    output.textContent = `R${result} is the interest total`
+}
+
+btnCalc.addEventListener('click', Calculate)
